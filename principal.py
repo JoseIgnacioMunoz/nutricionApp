6fff1b0f36472d7ba711cfc5c6241820a1beb57f
Ignacio <xjmunsot970@ieshnosmachado.org>
2023-10-12 19:09:18 +0200

    Creamos principal.py e importamos requests en él. También metemos en una variable la API key

---
+++ principal.py
@@ -0,0 +1,5 @@
+import requests #Importamos la biblioteca requests para hacer solicitudes a la API de Spoonacular.
+
+apiKey = "64e9a5cb1b1f41daa6a4b3a8c572d2dc" #Mi API key para utilizar la API
+
+urlInicial = "https://api.spoonacular.com/recipes"  #Esta en principio es la url base que voy a utilizar, más adelante aparecerán url distintas
