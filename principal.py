2a24b39994ceaeb6634cf0bba63238952e8b92ac
Ignacio <xjmunsot970@ieshnosmachado.org>
2023-10-14 17:33:27 +0200

    Entrada de datos por inputs al usuario

--- principal.py
+++ principal.py
@@ -2,18 +2,17 @@ import requests #Importamos la biblioteca requests para hacer solicitudes a la A
 import json
 
 apiKey = "64e9a5cb1b1f41daa6a4b3a8c572d2dc"
-
 urlInicial = "https://api.spoonacular.com/recipes"  #Esta en principio es la url base que voy a utilizar
-
-
-
 terminacionUrl = "/findByIngredients"
+
+ingredientes = input("Ingresa los ingredientes separados por comas: ")
+numRecetas = input("Número de recetas a obtener: ")
+
 parametros = {      #Variable que guardará el diccionario de parámetros a pasar al método requests.get
         "apiKey": apiKey,
-        "ingredients": "milk, berries",  # Ingredientes (la API espera que estén separados por comas)
-        "number": 5     # Cantidad de recetas a obtener
+        "ingredients":  ingredientes,  # Ingredientes (la API espera que estén separados por comas)
+        "number": numRecetas     # Cantidad de recetas a obtener
 }
-
 
 respuesta = requests.get(urlInicial + terminacionUrl, params=parametros)    #Se usará para control de respuesta de la API
 
@@ -23,5 +22,7 @@ if respuesta.status_code == 200:    #Aquí vemos si devuelve 200 OK con el statu
             for receta in recetas:      #Iteramos los objetos receta
              print(f"Nombre: {receta['title']}")
              print("")
+        else:
+            print("No se encontraron recetas con los ingredientes proporcionados.")
 else:
         print("No se pudieron buscar recetas. Lo lamentamos!", respuesta.status_code)   #Códigos de error. Puede ser por la cuota de la API o porque no haya conexion.
